9ea95bdc8907824654c6488551988d5c422b6ec5
Fady <fadyelferoon@gmail.com>
2026-04-29 21:27:10 +0300

    Update signals.js

--- routes/signals.js
+++ routes/signals.js
@@ -2,16 +2,8 @@ const express = require('express');
 const router = express.Router();
 const signalsController = require('../controllers/signalsController');
 
-router.post('/mxn',         signalsController.generateMXNSignals);
-router.get('/upcoming',     signalsController.getUpcomingSignals);
+router.post('/mxn', signalsController.generateMXNSignals);
+router.get('/upcoming', signalsController.getUpcomingSignals);
 router.post('/clear-cache', signalsController.clearCache);
-
-// Only add status/refresh if controller supports them
-if (signalsController.getCacheStatus) {
-  router.get('/status', signalsController.getCacheStatus);
-}
-if (signalsController.forceRefresh) {
-  router.post('/refresh', signalsController.forceRefresh);
-}
 
 module.exports = router;
